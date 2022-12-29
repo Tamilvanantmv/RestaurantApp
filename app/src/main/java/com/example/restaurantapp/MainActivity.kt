24285e19e84fef4b627efdee5b6dd08933cd68ff
Tamil <tamilvanantmv@gmail.com>
2022-12-29 23:10:41 +0530

    Restaurant App list

---
+++ app/src/main/java/com/example/restaurantapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.restaurantapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.restaurantapp.ui.theme.RestaurantAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RestaurantAppTheme {
+                // A surface container using the 'background' color from the theme
+                RestaurantAppTheme {
+                    RestaurantsScreen()
+                }
+            }
+        }
+    }
+}
